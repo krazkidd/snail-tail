3bd3ba6ed10d35540c3d48f06a079526734d8194
Mark Ross <krazkidd@gmail.com>
2023-11-11 13:42:27 -0700

    Add event which fires when tail catches user.

--- src/app/move/move.page.ts
+++ src/app/move/move.page.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 import { ConfigService } from '../services/config.service';
+import { StepCounterService } from '../services/step-counter.service';
 
 import { Config } from '../config';
 
@@ -12,12 +13,16 @@ import { Config } from '../config';
 export class MovePage implements OnInit {
   config: Config = this.configService.config;
 
-  constructor(private configService: ConfigService) {
+  isUserCaught = this.stepCounterService.isUserCaught;
+
+  constructor(private configService: ConfigService, private stepCounterService: StepCounterService) {
 
   }
 
   ngOnInit() {
-
+    this.stepCounterService.userCaught.subscribe(() => {
+      this.isUserCaught = this.stepCounterService.isUserCaught;
+    });
   }
 
 }
--- src/app/services/step-counter.service.ts
+++ src/app/services/step-counter.service.ts
@@ -6,20 +6,33 @@ import { EventEmitter, Injectable, Output } from '@angular/core';
 export class StepCounterService {
   @Output()
   stepsCounted = new EventEmitter<{ userSteps: number, tailSteps: number }>()
+  @Output()
+  userCaught = new EventEmitter()
 
   //TODO get from hardware step counter
   userSteps: number = 100;
   tailSteps: number = 0;
 
-  intervalId: number | null = null;
+  isUserCaught = false;
+
+  _intervalId: number | undefined = undefined;
 
   constructor() {
-    //const isAvailable = Capacitor.isPluginAvailable('TODO step counter');
+    this._intervalId = setInterval(() => {
+      //TODO poll hardware step counter
+      //const isAvailable = Capacitor.isPluginAvailable('TODO step counter');
 
-    this.intervalId = setInterval(() => {
       this.tailSteps++;
 
       this.stepsCounted.emit({ userSteps: this.userSteps, tailSteps: this. tailSteps });
+
+      if (this.tailSteps >= this.userSteps) {
+        clearInterval(this._intervalId);
+
+        this.isUserCaught = true;
+
+        this.userCaught.emit();
+      }
     }, 1000);
   }
 
