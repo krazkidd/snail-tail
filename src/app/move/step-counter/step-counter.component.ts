d51c3f29776fd8cb18d1f2001a15a784cf01adc3
Mark Ross <krazkidd@gmail.com>
2023-11-11 13:23:00 -0700

    Remove unused config from step counter service.

--- src/app/move/step-counter/step-counter.component.ts
+++ src/app/move/step-counter/step-counter.component.ts
@@ -3,20 +3,16 @@ import { Component, OnInit } from '@angular/core';
 import { ConfigService } from '../../services/config.service';
 import { StepCounterService } from '../../services/step-counter.service';
 
-import { Config } from '../../config.model';
-
 @Component({
   selector: 'app-step-counter',
   templateUrl: './step-counter.component.html',
   styleUrls: ['./step-counter.component.scss'],
 })
 export class StepCounterComponent  implements OnInit {
-  config: Config = this.configService.config;
-
   userSteps: number = this.stepCounter.userSteps;
   tailSteps: number = this.stepCounter.tailSteps;
 
-  constructor(private configService: ConfigService, private stepCounter: StepCounterService)
+  constructor(private stepCounter: StepCounterService)
   {
 
   }
