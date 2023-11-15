a4622e1b001c85763debead5f974b0218a01a58f
Mark Ross <krazkidd@gmail.com>
2023-11-15 12:08:41 -0700

    Cleanup.

--- src/app/services/step-counter.service.ts
+++ src/app/services/step-counter.service.ts
@@ -34,25 +34,24 @@ export class StepCounterService {
   }
 
   async startChase() {
-    const config = await this.config;
-
-    //TODO we shouldn't have to .find() the avatar; put this in ConfigService
-    const selectedTailAvatar = AVATARS_TAIL.find(t => t.icon === config.tailIcon)!;
-
     //TODO we can determine initial steps by defining some "head start" time like some number of days
     this.userSteps = 100;
     this.tailSteps = 0;
 
     this.isUserCaught = false;
 
-    // how long it takes the tail to cover the user's stride length
-    const tailStepTime_m = config.userStrideLength_m / (selectedTailAvatar.velocityKph * 1000) * 60;
-
-
     //TODO timing the first emit is a hack--subscribers can call async getStatus() themselves?
     setTimeout(async () => {
       // run once to update subscribers
       this.tailStep(true);
+
+      const config = await this.config;
+
+      //TODO we shouldn't have to .find() the avatar; put this in ConfigService
+      const selectedTailAvatar = AVATARS_TAIL.find(t => t.icon === config.tailIcon)!;
+
+      // how long it takes the tail to cover the user's stride length
+      const tailStepTime_m = config.userStrideLength_m / (selectedTailAvatar.velocityKph * 1000) * 60;
 
       this._intervalId = setInterval(() => this.tailStep(), tailStepTime_m * 60 * 1000);
     }, 1000);
