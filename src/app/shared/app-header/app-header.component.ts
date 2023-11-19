aff2e6a1c9e53c35aa48c4a4507330b335de13b3
Mark Ross <krazkidd@gmail.com>
2023-11-19 10:24:42 -0700

    Add step counter controls to header.
    Don't start automatically.

--- src/app/shared/app-header/app-header.component.ts
+++ src/app/shared/app-header/app-header.component.ts
@@ -1,5 +1,7 @@
 import { Component, Input, OnInit } from '@angular/core';
 
+import { StepCounterService } from 'src/app/services/step-counter.service';
+
 @Component({
   selector: 'app-header',
   templateUrl: './app-header.component.html',
@@ -12,7 +14,9 @@ export class AppHeaderComponent  implements OnInit {
   })
   title: string = '';
 
-  constructor() {
+  timerStatus$ = this.stepCounterService.timerState$;
+
+  constructor(protected stepCounterService: StepCounterService) {
 
   }
 
@@ -20,4 +24,23 @@ export class AppHeaderComponent  implements OnInit {
 
   }
 
+  onClick(action: string) {
+    switch (action) {
+      case 'start':
+        this.stepCounterService.startChase();
+
+        break;
+      case 'pause':
+        this.stepCounterService.pauseChase();
+
+        break;
+      case 'stop':
+        this.stepCounterService.stopChase();
+
+        break;
+      default:
+        // do nothing
+        break;
+    }
+  }
 }
