c3b1b152f0a0795a26650312edbec0adadd8b80c
Mark Ross <krazkidd@gmail.com>
2023-11-20 12:04:29 -0700

    Initial pass of persisting step counter state.
    There are two issues:
    1. Pause/resume doesn't reset the last step time on unpause.
    2. Refreshing page while active shows the initial state instead of a more recent state (not a bug but is a usability issue).

--- src/app/services/step-counter.service.ts
+++ src/app/services/step-counter.service.ts
@@ -1,9 +1,12 @@
 import { Injectable, OnDestroy } from '@angular/core';
 import { ReplaySubject, Subscription, firstValueFrom } from 'rxjs';
 
+import { StorageService } from './storage.service';
 import { ConfigService } from './config.service';
 
 import { AVATARS_TAIL } from '../constants';
+
+const STORAGE_KEY = 'step_counter';
 
 @Injectable({
   providedIn: 'root'
@@ -20,17 +23,60 @@ export class StepCounterService implements OnDestroy {
 
   private userSteps = 0;
   private tailSteps = 0;
+  private lastStepTimestamp = 0;
+
   private isUserCaught = false;
 
   private _configSub: Subscription | null = null;
   private _intervalId: number | undefined = undefined;
 
-  constructor(private configService: ConfigService) {
+  constructor(private storageService: StorageService, private configService: ConfigService) {
+    setTimeout(async () => {
+      const config = await firstValueFrom(this.configService.config$);
 
+      // how long it takes the tail to cover the user's stride length
+      const tailStepTime_m = config.userStrideLength_m / (AVATARS_TAIL[config.tailIcon].velocity_kph * 1000) * 60;
+      const tailStepTime_ms = tailStepTime_m * 60 * 1000;
+
+      const { userSteps, tailSteps, lastStepTimestamp, timerState } = await this.getSteps();
+
+      this.userSteps = userSteps;
+      this.tailSteps = tailSteps;
+      this.lastStepTimestamp = lastStepTimestamp;
+      this.isUserCaught = this.tailSteps >= this.userSteps;
+
+      // update subscribers with initial values
+      this.stepsCounted$.next({
+        userSteps: this.userSteps,
+        tailSteps: this.tailSteps,
+        isUserCaught: this.isUserCaught,
+        estimatedTimeRemaining_m: Math.floor((this.userSteps - this.tailSteps) * tailStepTime_m),
+      });
+
+      this.changeMode(timerState);
+    });
   }
 
   ngOnDestroy() {
     this.changeMode('stopped');
+  }
+
+  async getSteps() {
+    return await this.storageService.get(STORAGE_KEY) || {
+      userSteps: 0,
+      tailSteps: 0,
+      lastStepTimestamp: 0,
+      timerState: 'stopped',
+    };
+  }
+
+  async setSteps() {
+    return this.storageService.set(STORAGE_KEY, {
+      userSteps: this.userSteps,
+      tailSteps: this.tailSteps,
+      lastStepTimestamp: this.lastStepTimestamp,
+      timerState: await firstValueFrom(this.timerState$),
+    });
   }
 
   changeMode(timerState: 'started' | 'paused' | 'stopped') {
@@ -43,6 +89,8 @@ export class StepCounterService implements OnDestroy {
     }
 
     this.timerState$.next(timerState);
+
+    this.setSteps();
   }
 
   startChase() {
@@ -57,6 +105,7 @@ export class StepCounterService implements OnDestroy {
       if (this.isUserCaught || await firstValueFrom(this.timerState$) === 'stopped') {
         this.userSteps = config.initialLead_km * 1000 / config.userStrideLength_m;
         this.tailSteps = 0;
+        this.lastStepTimestamp = Date.now();
         this.isUserCaught = false;
 
         // update subscribers with initial values
@@ -66,9 +115,9 @@ export class StepCounterService implements OnDestroy {
           isUserCaught: this.isUserCaught,
           estimatedTimeRemaining_m: Math.floor((this.userSteps - this.tailSteps) * tailStepTime_m),
         });
+      } else if (await firstValueFrom(this.timerState$) === 'paused') {
+        this.lastStepTimestamp = Date.now();
       }
-
-      let lastStepTimestamp = Date.now();
 
       this._intervalId = setInterval(() => {
         const now = Date.now();
@@ -79,8 +128,7 @@ export class StepCounterService implements OnDestroy {
 
         if (tailSteps > 0) {
           this.tailSteps += tailSteps;
-
-          lastStepTimestamp += tailStepTime_ms * tailSteps;
+          this.lastStepTimestamp += tailStepTime_ms * tailSteps;
 
           if (this.tailSteps >= this.userSteps) {
             this.isUserCaught = true;
