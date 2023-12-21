9c5eb1cbe2c3fa8fe04d1c530a43015ff9cfb353
Mark Ross <krazkidd@gmail.com>
2023-12-20 19:47:22 -0700

    Update app ID.

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'net.xericode.snail-tail',
+  appId: 'net.xericode.app.snail-tail',
   appName: 'Snail Tail',
   webDir: 'www',
   server: {
